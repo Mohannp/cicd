04bdb020615362a32ab6daaa84643d9b1fd9e2fa
Mohannp <manunp40@gmail.com>
2019-11-19 09:33:35 +0530

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,8 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class test {
+  public static void main(String[] args) {
+    System.out.println("This is my test file");
+  }
+}
